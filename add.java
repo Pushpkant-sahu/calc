fc7148987183a8ac6f936705b5f1b10ed05f33c3
Pushpkant-sahu <145490508+Pushpkant-sahu@users.noreply.github.com>
2025-04-03 15:25:35 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,14 +1,14 @@
 mport java.util.Scanner;
 import java.lang.*;
-class Add{
+class mul{
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
-System.out.println("enter numbers to add ");
+System.out.println("enter numbers to multiply ");
 int i=sc.nextInt();
 int j=sc.nextInt();
 
-int k=i+j;
-System.out.println("addition : "+k);
+int k=i*j;
+System.out.println("multiplication  : "+k);
 sc.close();
 }
 
