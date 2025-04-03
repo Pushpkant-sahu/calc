99a6e40118dc7cc773d70007b344dcd241bda268
pushpkant <pushpkant@sahu.com>
2025-04-03 15:20:40 +0530

    add.java committed

---
+++ add.java
@@ -0,0 +1,16 @@
+mport java.util.Scanner;
+import java.lang.*;
+class Add{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter numbers to add ");
+int i=sc.nextInt();
+int j=sc.nextInt();
+
+int k=i+j;
+System.out.println("addition : "+k);
+sc.close();
+}
+
+}
+
